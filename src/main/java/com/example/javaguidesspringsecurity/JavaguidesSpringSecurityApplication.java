5ca2ebf5d3b6ffc6296a8f6cee6e40beb2c1d9d5
wojgeb <testsubject07@protonmail.com>
2023-05-02 21:19:40 +0200

    Initial commit

---
+++ src/main/java/com/example/javaguidesspringsecurity/JavaguidesSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaguidesspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaguidesSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguidesSpringSecurityApplication.class, args);
+	}
+
+}
