8fc4e9e455bc89c2565965c63421dbdff140e102
wojgeb <testsubject07@protonmail.com>
2023-05-06 22:15:16 +0200

    logging setup

--- src/main/java/com/example/javaguidesspringsecurity/JavaguidesSpringSecurityApplication.java
+++ src/main/java/com/example/javaguidesspringsecurity/JavaguidesSpringSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaguidesSpringSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaguidesSpringSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaguidesSpringSecurityApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/javaguidesspringsecurity/JavaguidesSpringSecurityApplicationTests.java
+++ src/test/java/com/example/javaguidesspringsecurity/JavaguidesSpringSecurityApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaguidesSpringSecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
