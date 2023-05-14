9ccd0b15c49f33cfd3ab813e3f7a55a54ccbd409
wojgeb <testsubject07@protonmail.com>
2023-05-14 21:11:50 +0200

    repositories implemented

--- src/main/java/com/example/javaguidesspringsecurity/RoleRepository.java
+++ src/main/java/com/example/javaguidesspringsecurity/RoleRepository.java
@@ -1,4 +1,174 @@
 package com.example.javaguidesspringsecurity;
 
-public class RoleRepository {
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class RoleRepository implements JpaRepository<Role, Long> {
+
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Role> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<Role> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    /**
+     * @param aLong
+     * @deprecated
+     */
+    @Override
+    public Role getOne(Long aLong) {
+        return null;
+    }
+
+    /**
+     * @param aLong
+     * @deprecated
+     */
+    @Override
+    public Role getById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Role getReferenceById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Role> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Role> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Role, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Role> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Role> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public List<Role> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Role> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Role entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Role> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<Role> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Role> findAll(Pageable pageable) {
+        return null;
+    }
 }
