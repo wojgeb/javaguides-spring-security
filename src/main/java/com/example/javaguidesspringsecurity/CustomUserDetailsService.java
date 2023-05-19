f3a6dfc9f6d0b20cb4a998aa6046c6117686a8a6
wojgeb <testsubject07@protonmail.com>
2023-05-19 22:59:41 +0200

    deleted interface

--- src/main/java/com/example/javaguidesspringsecurity/CustomUserDetailsService.java
+++ src/main/java/com/example/javaguidesspringsecurity/CustomUserDetailsService.java
@@ -1,4 +1,9 @@
 package com.example.javaguidesspringsecurity;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class CustomUserDetailsService {
 }
